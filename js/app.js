cd5dcf95916c7f25f5d2d2fe665899374cdb426f
Carl Jerrit Aquino <jerritt.aquino@gmail.com>
2018-10-02 11:05:21 -0400

    create open card function

--- js/app.js
+++ js/app.js
@@ -45,8 +45,7 @@ const deck_element = document.querySelector(".deck");
 
      //create icon element for card with appropriate class
      const newIcon = document.createElement('i');
-     newIcon.setAttribute('class','fa');
-     newIcon.setAttribute('class',card);
+     newIcon.setAttribute('class','fa ' + card);
 
      //append icon to li
      newCard.appendChild(newIcon);
@@ -72,3 +71,13 @@ shuffleDeck(deck);
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ function openCard(e) {
+   if(e.target.nodeName === 'LI') {
+     e.target.classList.add('show','open');
+   }
+ };
+
+deck_element.addEventListener('click', function(e){
+  openCard(e);
+});
